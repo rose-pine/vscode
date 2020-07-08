bc02805827e61897a15140e697c506ef35bd2f35
mellow <mvllow@icloud.com>
2020-07-08 16:20:34 -0500

    wip: add pinecone

---
+++ pinecone.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  input: "./themes/theme.json",
+  dir: "./themes",
+  prefix: "_",
+  themes: [
+    {
+      name: "Rosé Pine Dusk",
+      slug: "rose-pine-dusk",
+      type: "dark",
+      colors: {
+        accent1: "#efefef",
+      },
+    },
+    {
+      name: "Rosé Pine Dawn",
+      slug: "rose-pine-dawn",
+      type: "light",
+      colors: {
+        accent1: "#fa8072",
+      },
+    },
+  ],
+};
