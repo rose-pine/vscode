b5ad07d1eef8bc5cf0ace0a329711589a7ef3ec7
not <mvllow@icloud.com>
2021-01-05 16:56:44 -0600

    feat: use new pinecone-cli (#24)
    * feat: update for new pinecone-cli
    
    * chore: remove changelog

--- pinecone.config.js
+++ pinecone.config.js
@@ -1,133 +1,167 @@
+const alpha = (color, value) => {
+  let hex = Math.round(value * 255).toString(16)
+  let opacity = hex.length == 1 ? `0${hex}` : hex // eg. convert 5% from 0xd to 0x0d
+
+  return color + opacity
+}
+console.log('TEST', alpha('#e0def4', 0.25))
+
+let accents = {
+  rose: {
+    base: '#ebbcba',
+    moon: '#ea9a97',
+    dawn: '#e09694',
+  },
+  pine: {
+    base: '#31748f',
+    moon: '#3e8fb0',
+    dawn: '#286983',
+  },
+  foam: {
+    base: '#9ccfd8',
+    moon: '#9ccfd8',
+    dawn: '#619fa9',
+  },
+  iris: {
+    base: '#c4a7e7',
+    moon: '#c4a7e7',
+    dawn: '#9a86b1',
+  },
+  love: {
+    base: '#eb6f92',
+    moon: '#eb6f92',
+    dawn: '#bd6d83',
+  },
+  leaf: {
+    base: '#f6c177',
+    moon: '#f6c177',
+    dawn: '#769b94',
+  },
+  gold: {
+    base: '#f6c177',
+    moon: '#f6c177',
+    dawn: '#edac53',
+  },
+}
+
+let mutedAccents = {}
+
+// create muted shades of each accent colour
+Object.keys(accents).forEach((accent) => {
+  let { base, moon, dawn } = accents[accent]
+
+  mutedAccents[`${accent}-muted`] = {
+    base: alpha(base, 0.5),
+    moon: alpha(moon, 0.5),
+    dawn: alpha(dawn, 0.5),
+  }
+})
+
 module.exports = {
-  themes: [
-    {
-      name: 'Rosé Pine Dawn',
-      slug: 'rp-dawn',
-      type: '',
-      colors: {
-        transparent: '#0000',
-        'debug-bg': '#769b94',
-        'debug-fg': '#fff',
-        'debug-bg-prominent': '#575279A6',
-        'debug-fg-prominent': '#fff',
-        overlay: '#57527933',
-
-        border: '#fffdfa',
-        surface: '#fffaf3',
-        base: '#faf4ed',
-        shadow: '#f2e9de',
-
-        fg: '#575279',
-        'fg-subtle': '#575279cc',
-        'fg-inactive': '#57527980',
-        'fg-inactive-symbol': '#57527966',
-        'fg-highlight': '#57527980',
-        'bg-highlight-inactive': '#5752790D',
-        'bg-highlight': '#57527914',
-        'bg-highlight-match': '#9a86b140',
-        'bg-highlight-active': '#57527921',
-
-        rose: '#e09694',
-        'rose-muted': '#e0969480',
-        pine: '#286983',
-        'pine-muted': '#28698380',
-        foam: '#619fa9',
-        'foam-muted': '#619fa980',
-        iris: '#9a86b1',
-        'iris-muted': '#9a86b180',
-        love: '#bd6d83',
-        'love-muted': '#bd6d8380',
-        leaf: '#769b94',
-        'leaf-muted': '#769b9480',
-        gold: '#edac53',
-        'gold-muted': '#edac5380',
+  options: {
+    writeMeta: true,
+  },
+  theme: {
+    variants: {
+      base: {
+        name: 'Rosé Pine',
+        type: 'dark',
+      },
+      moon: {
+        name: 'Rosé Pine Moon',
+        type: 'dark',
+      },
+      dawn: {
+        name: 'Rosé Pine Dawn',
+        type: 'light',
       },
     },
-    {
-      name: 'Rosé Pine Moon',
-      slug: 'rp-moon',
-      type: 'dark',
-      colors: {
-        transparent: '#0000',
-        'debug-bg': '#3E8Fb0',
-        'debug-fg': '#fff',
-        'debug-bg-prominent': '#e0def433',
-        'debug-fg-prominent': '#fff',
-        overlay: '#e0def433',
-
-        border: '#393552',
-        surface: '#26233a',
+    colors: {
+      transparent: '#0000',
+      'debug-fg': '#fff',
+      'debug-fg-prominent': '#fff',
+      'debug-bg': {
+        base: '#3e8fb0',
+        moon: '#3e8fb0',
+        dawn: '#769b94',
+      },
+      'debug-bg-prominent': {
+        base: alpha('#e0def4', 0.25),
+        moon: alpha('#e0def4', 0.2),
+        dawn: alpha('#575279', 0.65),
+      },
+      overlay: {
+        base: alpha('#e0def4', 0.25),
+        moon: alpha('#e0def4', 0.2),
+        dawn: alpha('#575279', 0.2),
+      },
+      border: {
+        base: '#26233a',
+        moon: '#393552',
+        dawn: '#fffdfa',
+      },
+      surface: {
         base: '#1f1d2e',
-        shadow: '#0101014d',
-
-        fg: '#e0def4',
-        'fg-subtle': '#8C8AA7',
-        'fg-inactive': '#8C8AA760',
-        'fg-inactive-symbol': '#8C8AA780',
-        'fg-highlight': '#8C8AA780',
-        'bg-highlight-inactive': '#8C8AA71a',
-        'bg-highlight': '#8C8AA71F',
-        'bg-highlight-match': '#C4A7E740',
-        'bg-highlight-active': '#8C8AA72a',
-
-        rose: '#EA9A97',
-        'rose-muted': '#EA9A9780',
-        pine: '#3E8FB0',
-        'pine-muted': '#3E8FB080',
-        foam: '#9ccfd8',
-        'foam-muted': '#9ccfd880',
-        iris: '#c4a7e7',
-        'iris-muted': '#c4a7e780',
-        love: '#eb6f92',
-        'love-muted': '#eb6f9280',
-        leaf: '#f6c177',
-        'leaf-muted': '#f6c17780',
-        gold: '#f6c177',
-        'gold-muted': '#f6c17780',
+        moon: '#26233a',
+        dawn: '#fffaf3',
       },
-    },
-    {
-      name: 'Rosé Pine',
-      slug: 'rp',
-      type: 'dark',
-      colors: {
-        transparent: '#0000',
-        'debug-bg': '#3E8Fb0',
-        'debug-fg': '#fff',
-        'debug-bg-prominent': '#e0def440',
-        'debug-fg-prominent': '#fff',
-        overlay: '#e0def440',
-
-        border: '#26233a',
-        surface: '#1f1d2e',
+      base: {
         base: '#191724',
-        shadow: '#0101014d',
-
-        fg: '#e0def4',
-        'fg-subtle': '#e0def466',
-        'fg-inactive': '#706e86',
-        'fg-inactive-symbol': '#706e8680',
-        'fg-highlight': '#706e8666',
-        'bg-highlight-inactive': '#706e861a',
-        'bg-highlight': '#706e8633',
-        'bg-highlight-match': '#c4a7e740',
-        'bg-highlight-active': '#706e864D',
-
-        rose: '#ebbcba',
-        'rose-muted': '#ebbcba80',
-        pine: '#31748f',
-        'pine-muted': '#31748f80',
-        foam: '#9ccfd8',
-        'foam-muted': '#9ccfd880',
-        iris: '#c4a7e7',
-        'iris-muted': '#c4a7e780',
-        love: '#eb6f92',
-        'love-muted': '#eb6f9280',
-        leaf: '#f6c177',
-        'leaf-muted': '#f6c17780',
-        gold: '#f6c177',
-        'gold-muted': '#f6c1771a',
+        moon: '#1f1d2e',
+        dawn: '#faf4ed',
       },
+      shadow: {
+        base: alpha('#010101', 0.3),
+        moon: alpha('#010101', 0.3),
+        dawn: '#f2e9de',
+      },
+      fg: {
+        base: '#e0def4',
+        moon: '#e0def4',
+        dawn: '#575279',
+      },
+      'fg-subtle': {
+        base: alpha('#e0def4', 0.4),
+        moon: '#8c8aa7',
+        dawn: alpha('#575279', 0.8),
+      },
+      'fg-inactive': {
+        base: '#706e86',
+        moon: alpha('#8c8aa7', 0.38),
+        dawn: alpha('#575279', 0.5),
+      },
+      'fg-inactive-symbol': {
+        base: alpha('#706e86', 0.5),
+        moon: alpha('#8c8aa7', 0.5),
+        dawn: alpha('#575279', 0.4),
+      },
+      'fg-highlight': {
+        base: alpha('#706e86', 0.4),
+        moon: alpha('#8c8aa7', 0.5),
+        dawn: alpha('#575279', 0.5),
+      },
+      'bg-highlight-inactive': {
+        base: alpha('#706e86', 0.1),
+        moon: alpha('#8c8aa7', 0.1),
+        dawn: alpha('#575279', 0.05),
+      },
+      'bg-highlight': {
+        base: alpha('#706e86', 0.2),
+        moon: alpha('#8c8aa7', 0.12),
+        dawn: alpha('#575279', 0.08),
+      },
+      'bg-highlight-match': {
+        base: alpha('#c4a7e7', 0.25),
+        moon: alpha('#c4a7e7', 0.25),
+        dawn: alpha('#9a86b1', 0.25),
+      },
+      'bg-highlight-active': {
+        base: alpha('#706e86', 0.3),
+        moon: alpha('#8c8aa7', 0.17),
+        dawn: alpha('#575279', 0.13),
+      },
+      ...accents,
+      ...mutedAccents,
     },
-  ],
+  },
 }
