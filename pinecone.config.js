3659243f05f1576e66a8ac9bd98b912e5eb21500
fvrests <fvrests@icloud.com>
2021-01-22 21:56:43 -0600

    Feat update palette (#27)
    * test: init ava
    
    * test: check common pitfalls
    
    * chore: add watch script
    
    * chore: remove vscode tasks
    
    * feat: update palette & config
    
    * chore(deps): update lockfile
    
    * feat: update palette
    
    * feat: update moon backgrounds
    
    * chore: lowercase hex
    
    * feat: update dawn love
    
    * feat: add active tab background color
    
    * fix: add diff inserted color & allow highlighting
    
    * fix: moon surface & text contrast
    
    * feat: update file icons
    
    - add new `special` icon, currently for pinecone
    
    * update active tab background
    
    * update alpha to accept color object
    
    * test: check for invalid [object Object] color
    
    * use foam diff highlight
    
    * update love color - dawn variant
    
    * refactor: use lowercase color
    
    * feat: use pinecone alpha
    
    * build theme using new love color
    
    * update shadow colors
    
    * revert to gold for strings
    
    * lighten active tab background
    
    * v0.5.0
    
    Co-authored-by: mvllow <mvllow@icloud.com>

--- pinecone.config.js
+++ pinecone.config.js
@@ -1,15 +1,20 @@
-const alpha = (color, value) => {
-  let hex = Math.round(value * 255).toString(16)
-  let opacity = hex.length == 1 ? `0${hex}` : hex // eg. convert 5% from 0xd to 0x0d
-
-  return color + opacity
-}
+const { alpha } = require('./node_modules/pinecone-cli/dist')
 
 let accents = {
+  love: {
+    base: '#eb6f92',
+    moon: '#eb6f92',
+    dawn: '#b4637a',
+  },
+  gold: {
+    base: '#f6c177',
+    moon: '#f6c177',
+    dawn: '#ea9d34',
+  },
   rose: {
     base: '#ebbcba',
     moon: '#ea9a97',
-    dawn: '#e09694',
+    dawn: '#d7827e',
   },
   pine: {
     base: '#31748f',
@@ -19,27 +24,12 @@ let accents = {
   foam: {
     base: '#9ccfd8',
     moon: '#9ccfd8',
-    dawn: '#619fa9',
+    dawn: '#56949f',
   },
   iris: {
     base: '#c4a7e7',
     moon: '#c4a7e7',
-    dawn: '#9a86b1',
-  },
-  love: {
-    base: '#eb6f92',
-    moon: '#eb6f92',
-    dawn: '#bd6d83',
-  },
-  leaf: {
-    base: '#f6c177',
-    moon: '#f6c177',
-    dawn: '#769b94',
-  },
-  gold: {
-    base: '#f6c177',
-    moon: '#f6c177',
-    dawn: '#edac53',
+    dawn: '#907aa9',
   },
 }
 
@@ -47,13 +37,7 @@ let mutedAccents = {}
 
 // create muted shades of each accent colour
 Object.keys(accents).forEach((accent) => {
-  let { base, moon, dawn } = accents[accent]
-
-  mutedAccents[`${accent}-muted`] = {
-    base: alpha(base, 0.5),
-    moon: alpha(moon, 0.5),
-    dawn: alpha(dawn, 0.5),
-  }
+  mutedAccents[`${accent}Muted`] = alpha(accents[accent], 0.5)
 })
 
 module.exports = {
@@ -77,88 +61,70 @@ module.exports = {
     },
     colors: {
       transparent: '#0000',
-      'debug-fg': '#fff',
-      'debug-fg-prominent': '#fff',
-      'debug-bg': {
-        base: '#3e8fb0',
-        moon: '#3e8fb0',
-        dawn: '#769b94',
-      },
-      'debug-bg-prominent': {
-        base: alpha('#e0def4', 0.25),
-        moon: alpha('#e0def4', 0.2),
-        dawn: alpha('#575279', 0.65),
-      },
-      overlay: {
-        base: alpha('#e0def4', 0.25),
-        moon: alpha('#e0def4', 0.2),
-        dawn: alpha('#575279', 0.2),
-      },
-      border: {
-        base: '#26233a',
-        moon: '#393552',
-        dawn: '#fffdfa',
-      },
-      surface: {
-        base: '#1f1d2e',
-        moon: '#26233a',
-        dawn: '#fffaf3',
-      },
-      base: {
+
+      primary: accents.rose,
+      onPrimary: {
         base: '#191724',
-        moon: '#1f1d2e',
+        moon: '#26233a',
         dawn: '#faf4ed',
       },
-      shadow: {
-        base: alpha('#010101', 0.3),
-        moon: alpha('#010101', 0.3),
-        dawn: '#f2e9de',
-      },
-      fg: {
+
+      text: {
         base: '#e0def4',
         moon: '#e0def4',
         dawn: '#575279',
       },
-      'fg-subtle': {
-        base: alpha('#e0def4', 0.4),
-        moon: '#8c8aa7',
-        dawn: alpha('#575279', 0.8),
+      textSubtle: {
+        base: '#6e6a86',
+        moon: '#817c9c',
+        dawn: '#6e6a86',
       },
-      'fg-inactive': {
-        base: '#706e86',
-        moon: alpha('#8c8aa7', 0.38),
-        dawn: alpha('#575279', 0.5),
+      textIgnored: {
+        base: '#555169',
+        moon: '#59546d',
+        dawn: '#9893a5',
       },
-      'fg-inactive-symbol': {
-        base: alpha('#706e86', 0.5),
-        moon: alpha('#8c8aa7', 0.5),
-        dawn: alpha('#575279', 0.4),
+
+      bgBase: {
+        base: '#191724',
+        moon: '#232136',
+        dawn: '#faf4ed',
       },
-      'fg-highlight': {
-        base: alpha('#706e86', 0.4),
-        moon: alpha('#8c8aa7', 0.5),
-        dawn: alpha('#575279', 0.5),
+      bgSurface: {
+        base: '#1f1d2e',
+        moon: '#2a273f',
+        dawn: '#fffaf3',
       },
-      'bg-highlight-inactive': {
-        base: alpha('#706e86', 0.1),
-        moon: alpha('#8c8aa7', 0.1),
-        dawn: alpha('#575279', 0.05),
+
+      uiBorder: {
+        base: '#26233a',
+        moon: '#393552',
+        dawn: '#fffdfa',
       },
-      'bg-highlight': {
-        base: alpha('#706e86', 0.2),
-        moon: alpha('#8c8aa7', 0.12),
-        dawn: alpha('#575279', 0.08),
+      uiShadow: {
+        base: alpha('#010101', 0.1),
+        moon: alpha('#191724', 0.3),
+        dawn: alpha('#f2e9de', 0.3),
       },
-      'bg-highlight-match': {
-        base: alpha('#c4a7e7', 0.25),
-        moon: alpha('#c4a7e7', 0.25),
-        dawn: alpha('#9a86b1', 0.25),
+
+      highlight: {
+        base: alpha('#6e6a86', 0.2),
+        moon: alpha('#817c9c', 0.15),
+        dawn: alpha('#6e6a86', 0.08),
       },
-      'bg-highlight-active': {
-        base: alpha('#706e86', 0.3),
-        moon: alpha('#8c8aa7', 0.17),
-        dawn: alpha('#575279', 0.13),
+      highlightInactive: {
+        base: alpha('#6e6a86', 0.1),
+        moon: alpha('#817c9c', 0.08),
+        dawn: alpha('#6e6a86', 0.05),
       },
+      highlightOverlay: {
+        base: alpha('#6e6a86', 0.4),
+        moon: alpha('#817c9c', 0.3),
+        dawn: alpha('#6e6a86', 0.15),
+      },
+      diffHighlightInserted: alpha(accents.foam, 0.08),
+      diffHighlightRemoved: alpha(accents.love, 0.08),
+
       ...accents,
       ...mutedAccents,
     },
