6baf53356821eedacabfcb0e4cc31c3caffc5dc7
mvllow <mvllow@icloud.com>
2021-06-14 15:32:54 -0500

    update examples

--- examples/js-ts.ts
+++ examples/js-ts.ts
@@ -1,26 +1,21 @@
 interface Fruit {
   name: string
   isSweet: boolean
-  quantity: number
-  callback?: Function
 }
 
-function randomizeFruit(fruit: Fruit) {
-  const { name, isSweet, quantity, callback } = fruit
-  const newFruit: Fruit = {
-    name: name.split('').reverse().join(''),
-    isSweet: !isSweet,
-    quantity: Math.floor(Math.random() * (quantity + name.length)),
-  }
-
-  callback(newFruit)
-}
-
-randomizeFruit({
-  name: 'Pitaya',
+const whenToEatSweetFruits = 'all the time'
+const whenToEatSourFruits = 'all the time'
+const someFruit: Fruit = {
+  name: 'pitaya',
   isSweet: true,
-  quantity: 42,
-  callback: (fruit: Fruit) => {
-    console.log('Fruit randomized:', fruit)
-  },
-})
+}
+
+function printTheBestTimeToEatSweetOrSourFruits(fruit: Fruit) {
+  console.log(
+    `The best time to eat ${fruit.name} is ${
+      fruit.isSweet ? whenToEatSweetFruits : whenToEatSourFruits
+    }`
+  )
+}
+
+printTheBestTimeToEatSweetOrSourFruits(someFruit)
--- examples/svelte.svelte
+++ examples/svelte.svelte
@@ -1,7 +1,21 @@
-<h2>The best time to eat a sweet fruit is { whenToEatSweetFruits }!</h2>
+{#if someFruit.isSweet}
+    <h2>The best time to eat {someFruit.name} is { whenToEatSweetFruits }!</h2>
+{:else}
+    <h2>The best time to eat {someFruit.name} is { whenToEatSourFruits }!</h2>
+{/if}
 
 <script lang="ts">
+    interface Fruit {
+        name: string
+        isSweet: boolean
+    }
+
     const whenToEatSweetFruits = 'all the time'
+    const whenToEatSourFruits = 'all the time'
+    const someFruit: Fruit = {
+        name: "pitaya",
+        isSweet: true
+    }
 </script>
 
 <style>
