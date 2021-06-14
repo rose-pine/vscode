bfa61237dc3701761da0d2cb5e22ac9ebba9af68
mvllow <mvllow@icloud.com>
2021-06-14 15:32:54 -0500

    update examples

--- examples/vue.vue
+++ examples/vue.vue
@@ -1,9 +1,24 @@
 <template>
-  <h2>The best time to eat a sweet fruit is {{ whenToEatSweetFruits }}!</h2>
+  <h2 v-if="someFruit.isSweet">
+    The best time to eat {{ someFruit.name }} is {{ whenToEatSweetFruits }}!
+  </h2>
+  <h2 v-else>
+    The best time to eat {{ someFruit.name }} is {{ whenToEatSourFruits }}!
+  </h2>
 </template>
 
 <script setup lang="ts">
+interface Fruit {
+  name: string
+  isSweet: boolean
+}
+
 const whenToEatSweetFruits = 'all the time'
+const whenToEatSourFruits = 'all the time'
+const someFruit: Fruit = {
+  name: 'pitaya',
+  isSweet: true,
+}
 </script>
 
 <style scoped>
