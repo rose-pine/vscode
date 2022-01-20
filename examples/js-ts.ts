a8c25819a5450a19d6f12305a05f3bd69471d70f
mvllow <mvllow@icloud.com>
2022-01-20 12:00:58 -0600

    add prettier

--- examples/js-ts.ts
+++ examples/js-ts.ts
@@ -1,21 +1,21 @@
 interface Fruit {
-  name: string;
-  isSweet: boolean;
+	name: string
+	isSweet: boolean
 }
 
-const whenToEatSweetFruits = "all the time";
-const whenToEatSourFruits = "all the time";
+const whenToEatSweetFruits = 'all the time'
+const whenToEatSourFruits = 'all the time'
 const someFruit: Fruit = {
-  name: "pitaya",
-  isSweet: true,
-};
+	name: 'pitaya',
+	isSweet: true,
+}
 
 function printTheBestTimeToEatSweetOrSourFruits(fruit: Fruit) {
-  console.log(
-    `The best time to eat ${fruit.name} is ${
-      fruit.isSweet ? whenToEatSweetFruits : whenToEatSourFruits
-    }`
-  );
+	console.log(
+		`The best time to eat ${fruit.name} is ${
+			fruit.isSweet ? whenToEatSweetFruits : whenToEatSourFruits
+		}`
+	)
 }
 
-printTheBestTimeToEatSweetOrSourFruits(someFruit);
+printTheBestTimeToEatSweetOrSourFruits(someFruit)
