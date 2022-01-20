e411dfaef3430673619f1a67c2c526cfd09c6812
mvllow <mvllow@icloud.com>
2022-01-20 12:01:34 -0600

    update palette

--- pinecone.config.js
+++ pinecone.config.js
@@ -1,75 +1,86 @@
-import { alpha, defineConfig } from "pinecone-cli";
-import {
-  palette,
-  colorsByRole,
-  transparentHighlightsByRole,
-} from "@rose-pine/palette";
+import { alpha, defineConfig } from 'pinecone-cli'
+import { roles } from '@rose-pine/palette'
 
-const mutedColors = {};
+const colors = {}
+Object.keys(roles).map((role) => {
+	const currentRole = roles[role]
 
-Object.keys(colorsByRole).forEach((color) => {
-  mutedColors[`${color}Muted`] = alpha(colorsByRole[color], 0.5);
-});
+	colors[role] = {
+		main: currentRole.main.alpha
+			? currentRole.main.alpha.hex
+			: currentRole.main.hex,
+		moon: currentRole.moon.alpha
+			? currentRole.moon.alpha.hex
+			: currentRole.moon.hex,
+		dawn: currentRole.dawn.alpha
+			? currentRole.dawn.alpha.hex
+			: currentRole.dawn.hex,
+	}
+
+	colors[`${role}Muted`] = {
+		main: alpha(currentRole.main.hex, 0.5),
+		moon: alpha(currentRole.moon.hex, 0.5),
+		dawn: alpha(currentRole.dawn.hex, 0.5),
+	}
+})
 
 export default defineConfig({
-  source: "./themes/_pinecone-color-theme.json",
-  output: "./themes",
-  prefix: "_",
-  options: {
-    experimental: {
-      removeEmptyThemeValues: true,
-    },
-  },
-  theme: {
-    variants: {
-      dark: {
-        name: "Rosé Pine",
-        type: "dark",
-      },
-      moon: {
-        name: "Rosé Pine Moon",
-        type: "dark",
-      },
-      dawn: {
-        name: "Rosé Pine Dawn",
-        type: "light",
-      },
-    },
-    colors: {
-      transparent: "#0000",
-      unusedOpacity: alpha("#000", 0.5),
+	source: './themes/_pinecone-color-theme.json',
+	output: './themes',
+	prefix: '_',
+	options: {
+		experimental: {
+			removeEmptyThemeValues: true,
+		},
+	},
+	theme: {
+		variants: {
+			main: {
+				name: 'Rosé Pine',
+				type: 'dark',
+			},
+			moon: {
+				name: 'Rosé Pine Moon',
+				type: 'dark',
+			},
+			dawn: {
+				name: 'Rosé Pine Dawn',
+				type: 'light',
+			},
+		},
+		colors: {
+			transparent: '#0000',
+			unusedOpacity: alpha('#000', 0.5),
 
-      primary: palette.rose,
-      primaryHover: alpha(palette.rose, 0.9),
-      onPrimary: palette.base,
+			primary: colors.rose,
+			primaryHover: alpha(colors.rose, 0.9),
+			onPrimary: colors.base,
 
-      secondary: palette.iris,
-      secondaryHover: alpha(palette.iris, 0.9),
-      onSecondary: palette.base,
+			secondary: colors.iris,
+			secondaryHover: alpha(colors.iris, 0.9),
+			onSecondary: colors.base,
 
-      neutral: palette.surface,
-      neutralHover: palette.overlay,
-      onNeutral: palette.text,
+			neutral: colors.surface,
+			neutralHover: colors.overlay,
+			onNeutral: colors.text,
 
-      shadow: {
-        dark: alpha("#010101", 0.1),
-        moon: alpha("#191724", 0.3),
-        dawn: alpha("#f2e9de", 0.3),
-      },
+			shadow: {
+				main: alpha('#010101', 0.1),
+				moon: alpha('#191724', 0.3),
+				dawn: alpha('#f2e9de', 0.3),
+			},
 
-      diffHighlightInserted: alpha(palette.foam, 0.08),
-      diffHighlightRemoved: alpha(palette.love, 0.08),
+			diffHighlightInserted: alpha(colors.foam, 0.08),
+			diffHighlightRemoved: alpha(colors.love, 0.08),
 
-      icon: palette.subtle,
-      iconActive: palette.text,
+			icon: colors.subtle,
+			iconActive: colors.text,
 
-      information: palette.foam,
-      warning: palette.gold,
-      error: palette.love,
+			information: colors.foam,
+			warning: colors.gold,
+			error: colors.love,
 
-      ...colorsByRole,
-      ...transparentHighlightsByRole,
-      ...mutedColors,
-    },
-  },
-});
+			...colors,
+		},
+	},
+})
