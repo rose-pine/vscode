b5e90a938ffb8821be02fdaf09e904aaf48f07ca
mvllow <mvllow@icloud.com>
2023-02-01 17:12:17 -0500

    chore: revert prettier changes

--- pinecone.config.js
+++ pinecone.config.js
@@ -1,54 +1,54 @@
-import { colorish, defineConfig } from "pinecone-cli";
-import { roles } from "@rose-pine/palette";
+import { colorish, defineConfig } from 'pinecone-cli'
+import { roles } from '@rose-pine/palette'
 
-const palette = {};
+const palette = {}
 Object.keys(roles).map((role) => {
-	const currentRole = roles[role];
+	const currentRole = roles[role]
 
 	// Add Rosé Pine palette.
 	palette[role] = {
 		main: currentRole.main.alpha?.hex ?? currentRole.main.hex,
 		moon: currentRole.moon.alpha?.hex ?? currentRole.moon.hex,
 		dawn: currentRole.dawn.alpha?.hex ?? currentRole.dawn.hex,
-	};
+	}
 
 	// Add translucent variations.
 	palette[`${role}/50`] = {
 		main: colorish(currentRole.main.hex, 0.5),
 		moon: colorish(currentRole.moon.hex, 0.5),
 		dawn: colorish(currentRole.dawn.hex, 0.5),
-	};
+	}
 	palette[`${role}/15`] = {
 		main: colorish(currentRole.main.hex, 0.15),
 		moon: colorish(currentRole.moon.hex, 0.15),
 		dawn: colorish(currentRole.dawn.hex, 0.15),
-	};
-});
+	}
+})
 
 export default defineConfig({
 	options: {
-		source: "./themes/_pinecone-color-theme.json",
-		output: "./themes",
-		prefix: "$",
+		source: './themes/_pinecone-color-theme.json',
+		output: './themes',
+		prefix: '$',
 		includeNonItalicVariants: true,
 	},
 	variants: {
 		main: {
-			name: "Rosé Pine",
-			type: "dark",
+			name: 'Rosé Pine',
+			type: 'dark',
 		},
 		moon: {
-			name: "Rosé Pine Moon",
-			type: "dark",
+			name: 'Rosé Pine Moon',
+			type: 'dark',
 		},
 		dawn: {
-			name: "Rosé Pine Dawn",
-			type: "light",
+			name: 'Rosé Pine Dawn',
+			type: 'light',
 		},
 	},
 	colors: {
 		shadow: colorish(palette.surface, 0.3),
-		transparent: "#0000",
+		transparent: '#0000',
 
 		onPrimary: palette.base,
 		primary: palette.rose,
@@ -60,4 +60,4 @@ export default defineConfig({
 
 		...palette,
 	},
-});
+})
